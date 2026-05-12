89990e225824894e033f3f15d15d229b90b55a23
Swechchha Adhikari <swechchha.adhikari4@gmail.com>
2026-05-12 16:01:53 +0200

    size

---
+++ sizes.py
@@ -0,0 +1,6 @@
+# scheme        sk        pk        c      ss
+# frodokem640  19888     9616      9720    16
+# frodokem976  31296     15632     15744   24
+# frodokem1344 43088     21520     21632   32
+
+from kaiburr import KyberParameterSet, communication_costs
