f675e3380727104c5f3b7f45f6c40fc3f239b6a7
Swechchha Adhikari <swechchha.adhikari4@gmail.com>
2026-09-01 17:31:48 -0600

    more frodo api wrapper updates

--- src/benchmarks/frodo-include-1344/api.h
+++ src/benchmarks/frodo-include-1344/api.h
@@ -1,1 +1,5 @@
 #include "api_frodo1344.h"
+
+#define crypto_kem_keypair crypto_kem_keypair_Frodo1344
+#define crypto_kem_enc crypto_kem_enc_Frodo1344
+#define crypto_kem_dec crypto_kem_dec_Frodo1344
--- src/benchmarks/frodo-include-976/api.h
+++ src/benchmarks/frodo-include-976/api.h
@@ -1,1 +1,5 @@
 #include "api_frodo976.h"
+
+#define crypto_kem_keypair crypto_kem_keypair_Frodo976
+#define crypto_kem_enc crypto_kem_enc_Frodo976
+#define crypto_kem_dec crypto_kem_dec_Frodo976
